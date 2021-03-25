005b7caae9acc30544f5ab74fb52603359d932bf
TeXmeijin <i8858garden@gmail.com>
2021-03-26 00:20:56 +0900

    add: サイズが変わってConsole.logできるところと、Tailwindのルールをカスタマイズできるところまで

--- src/components/Arrow.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import '../styles/arrow.css'
-
-export const Arrow: React.VFC<{ left?: boolean }> = ({ left }) => {
-  return <div className={'arrow' + ' ' + (left ? 'left' : '')} />
-}
--- src/components/Execution.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Execution: React.VFC = () => {
-  return <div className="absolute bg-blue-50 w-full h-full border-4 border-blue-200" />
-}
