be6985927c0ab3f3eb8fedd2204203d1d164f550
TeXmeijin <i8858garden@gmail.com>
2021-10-30 09:43:26 +0900

    fix: prettierrc config delete unneeded config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,5 @@
 // .prettierrc.js
 module.exports = {
   printWidth: 120,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'avoid',
+  singleQuote: true
 }
